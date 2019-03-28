4dd4c480a16cadb75d153fa82b20fb7d65cbd0fe
kingflintandsteel <trevor.flint17@ncf.edu>
2019-03-28 07:52:26 -0400

    aocinformation and graballcourses functions completed but not tested

--- Website Docker/Helper_Functions/funtions1.py
+++ Website Docker/Helper_Functions/funtions1.py
@@ -1,18 +1,90 @@
 #-----------------------------------------------------------------------------
+#HELPER FOR THE HELPERS
+def connectCursor():
+	connection = mysql.connector.connect(user='root', password='bluecrew', database='Aoc')
+	cur = connection.cursor()
+	return (connection,cur)
+
+#-----------------------------------------------------------------------------
 #GENERAL HELPER FUNCTIONS
 
 def studentProgress(student):
 	#this function will calculate how much time a student needs to graduate
 	#returns a year that is the current year plus the number of years needed
 	#to graduatate at the current rate
+	#-----------------------------------
+	#ASSUMTIONS MADE DURING THIS CALCULATION
+	#1 - the student is taking for classes a semester
+	#2 - the student has at least 3 of those classes each semester in their AOC
+	#3 - all courses the student needs are being offered when they need/want them
 
 def aocInformation(AOC):
 	#this function will grab all information attached to an AOC, including name
 	#department, requirements, and courses for each requirement
 	#returns a tuple
+	connection, cur = connectCursor()
+	query = "SELECT name FROM AOCs WHERE id = %s"
+	values = (AOC,)
+	cur.execute(query,values)
+	results = cur.fetchall()
+	connection.commit()
+	cur.close()
+	cur = connection.cursor()
+	AOCname = ""
+
+	for name in results:
+		AOCname = name
+
+	query = "SELECT * FROM Requirements WHERE AOC_id = %s"
+	values = (AOC,)
+	cur.execute(query,values)
+	results = cur.fetchall()
+	connection.commit()
+	cur.close()
+	cur = connection.cursor()
+	tempReqList = []
+
+	for id,name,AOC_id,NUM_to_complete in results:
+		query = "SELECT Course_id FROM Courses_for_requirement WHERE Requirement_id = %s"
+		values = (id,)
+		cur.execute(query,values)
+		results2 = cur.fetchall()
+		connection.commit()
+		cur.close()
+		cur = connection.cursor()
+		Courses_for_requirementList = []
+
+		for Course_id in results2:
+			query = "SELECT name,Department_id FROM Courses WHERE id = %s"
+			values = (Course_id,)
+			cur.execute()
+			results3 = cur.fetchall()
+			connection.commit()
+			cur.close()
+			cur = connection.cursor()
+
+			for name,Department_id in results3:
+				Courses_for_requirementList.append((name,Department_id))
+
+		tempReqList.append((id,name,AOC_id,Courses_for_requirementList,NUM_to_complete))
+
+	return (AOCname,tempReqList)
 
 def grabAllCourses():
 	#this function will return a list of tuples of all course names and Id numbers
+	tempList = []
+	connection, cur = connectCursor()
+	query = "SELECT * FROM Courses"
+	cur.execute(query,)
+	results = cur.fetchall()
+	connection.commit()
+	cur.close()
+	cur = connection.cursor()
+
+	for id,name,Department_id in results:
+		tempList.append((id,name,Department_id))
+
+	return tempList
 
 
 #-----------------------------------------------------------------------------
