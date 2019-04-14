e075518f0f0451dec772dfb62d78ead1080719c4
kingflintandsteel <trevor.flint17@ncf.edu>
2019-04-14 19:39:06 -0400

    Update app.py

--- WebsiteDocker/app.py
+++ WebsiteDocker/app.py
@@ -2,6 +2,7 @@ from flask import Flask, session, render_template, redirect, request, url_for
 #import mysql.connector
 
 from Helper_Functions import funtions1
+from Helper_Functions.funtions1 import getStudentProfile, getstudentProgress
 
 import datetime as dt
 
